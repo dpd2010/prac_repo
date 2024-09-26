1e88f213913227a74162b373e8c180e009b51a1f
DPD2010 <63023368+dpd2010@users.noreply.github.com>
2024-09-26 18:45:25 +0000

    copied code from separarte source

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Burger menu toggle
+document.getElementById('burger-menu').addEventListener('click', () => {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  });
+  
